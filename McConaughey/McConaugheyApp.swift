418975c00ebce625d04353553fe08cc8cd1ce3fe
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-16 16:42:51 -0500

    Initial Commit

---
+++ McConaughey/McConaugheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  McConaugheyApp.swift
+//  McConaughey
+//
+//  Created by Nick Nedjat on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct McConaugheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
